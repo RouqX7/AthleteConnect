2976252d47187b13199f607c1db6c9dd93045de3
RouqX7 <farouq2169@gmail.com>
2025-03-14 18:15:10 +0000

    feat:started on profile

--- helpers/ModelMocks.ts
+++ helpers/ModelMocks.ts
@@ -1,42 +1,68 @@
-import { Profile } from "../types"
+import { Profile } from "../types";
 
 export const defaultProfile = (
-    email:string,
-    uid:string,
-    username:string,
-    firstName:string,
-    lastName:string,
+    email: string,
+    uid: string,
+    username: string,
+    firstName: string,
+    lastName: string,
+    profileType: "player" | "club"
 ): Profile => {
     return {
         verified: false,
-        accountStatus: 'inactive',
+        accountStatus: "inactive",
         lastUpdated: new Date(),
+        profileType,
         preferences: {
-            theme: 'light',
+            theme: "light",
             notifications: {
                 email: true,
                 sms: true,
                 push: true,
             },
-    },
-    user: {
-        authInfo: {
-            uid,
-            email,
-            username,
-            phone:"",
-            secureLogin:true,
-            lastLogin: new Date(),
-            createdAt: new Date(),
         },
-        firstName,
-        lastName,
-        bio: "",
-        image: "",
-        location:null,
-        socialLinks: {},
-        website: "",
-        isAgreed:false
-    },
-} as Profile;
+        user: {
+            authInfo: {
+                uid,
+                firstName,
+                lastName,
+                email,
+                username,
+                phone: "",
+                secureLogin: true,
+                lastLogin: new Date(),
+                createdAt: new Date(),
+            },
+            bio: "",
+            image: "",
+            location: null,
+            socialLinks: {},
+            website: "",
+            isAgreed: false,
+        },
+        ...(profileType === "player"
+            ? {
+                  player: {
+                      sports: "",
+                      club: null,
+                      role: "player",
+                      positions: [],
+                      socialLinks: {},
+                      images: [],
+                      websites: [],
+                      location: "",
+                  },
+              }
+            : {
+                  club: {
+                      name: "",
+                      sports: "",
+                      role: "manager",
+                      socialLinks: {},
+                      images: [],
+                      websites: [],
+                      location: "",
+                  },
+              }),
+    };
 };
--- types.ts
+++ types.ts
@@ -1,16 +1,23 @@
 export type User = {
     uid: string;
-    username?: string|null;
+    firstName: string;  
+    lastName: string;   
+    username?: string | null;
     email?: string;
-    phone?:string;
-    secureLogin?:boolean;
-    createdAt?: Date;         
-    lastLogin?: Date
-}
+    phone?: string;
+    userType?: string;
+    secureLogin?: boolean;
+    createdAt?: Date;
+    lastLogin?: Date;
+};
+
 
 export type Profile = {
     user:BasicUserInfo;
     verified:boolean;
+    profileType: 'player' | 'club';
+    player?: PlayerProfile;
+    club?: ClubProfile;
     accountStatus?: 'active' | 'inactive' | 'suspended'; // Optional: Status of the user account
     lastUpdated?: Date;                                  // Optional: Last profile update timestamp
     preferences?: UserPreferences;
@@ -20,9 +27,8 @@ export type Profile = {
 export type BasicUserInfo = {
     authInfo: User;
     image?:string|null;
-    firstName:string;
-    lastName:string;
-    bio?:string|null
+    club?:string | null;
+    bio?:string|null;
     isAgreed?: boolean|null;
     location?:LocationData|null;
     website?: string | null;               // Optional: Personal or company website
@@ -33,9 +39,9 @@ export type BasicUserInfo = {
 export type UserPreferences = {
     theme?: 'light' | 'dark';
     notifications?: {
-        email: boolean;
-        sms: boolean;
-        push: boolean;
+        email?: boolean;
+        sms?: boolean;
+        push?: boolean;
     };
     language?: string;                    // Optional: Preferred language for the app
 };
@@ -46,6 +52,27 @@ export type SocialLinks = {
     github?: string;
     facebook?: string;
     [key: string]: string | undefined;    // Allows for flexibility with other social networks
+};
+
+export type PlayerProfile = {
+    club?: string | null;  // Moved here
+    sports: string;
+    role?: 'player' | 'captain';
+    positions?:string[];
+    socialLinks?: SocialLinks;
+    images?: string[];
+    websites?: string[];
+    location?: string;
+};
+
+export type ClubProfile = {
+    name: string;
+    sports: string;
+    role?: 'owner' | 'manager' | 'staff' | 'coach';  // Added roles
+    socialLinks?: SocialLinks;
+    images?: string[];
+    websites?: string[];
+    location?: string;
 };
 
 export type LocationData = {
